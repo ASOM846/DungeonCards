8e3de67ee965ddb5baa801974e178ec4161e72ae
antoni <janikantoni@poczta.fm>
2026-06-19 12:43:06 +0200

    extracted button logic to new class

--- src/menu.hpp
+++ src/menu.hpp
@@ -1,45 +1,12 @@
 #pragma once
 
+#include "button.hpp"
 #include "textureManager.hpp"
-#include "ui.hpp"
 #include <raylib.h>
 
 enum class MenuUpdateResoult {
 	NONE,
 	PLAY,
-};
-
-struct Button {
-	Rectangle rect;
-	float shadowOffset;
-	const char *text;
-
-	Button() = default;
-	Button(Rectangle rectangle, const char *p_text) {
-		rect = rectangle;
-		text = p_text;
-	}
-
-	[[nodiscard]] bool IsClicked(const Vector2 mousePos) const {
-		return (CheckCollisionPointRec(mousePos, rect) &&
-				IsMouseButtonPressed(MOUSE_BUTTON_LEFT));
-	}
-
-	[[nodiscard]] bool IsHovered(const Vector2 mousePos) const {
-		return (CheckCollisionPointRec(mousePos, rect));
-	}
-
-	void Update(Vector2 mousePos) {
-		shadowOffset = 4.0f;
-		if (IsHovered(mousePos)) {
-			rect.y -= 12.0f;
-			shadowOffset = 14.0f;
-		}
-	}
-
-	void Draw(TextureManager &tm) const {
-		Ui::DrawMessageRect(rect, text, shadowOffset, tm.getCustonFont());
-	}
 };
 
 class Menu {
